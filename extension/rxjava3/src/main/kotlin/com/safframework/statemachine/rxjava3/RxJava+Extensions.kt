6a889e1cc39df1aa6f6684cc865548af1fde8ee9
fengzhizi715 <fengzhizi715@126.com>
2021-01-04 17:35:00 +0800

    add rxjava3 module

---
+++ extension/rxjava3/src/main/kotlin/com/safframework/statemachine/rxjava3/RxJava+Extensions.kt
@@ -0,0 +1,51 @@
+package com.safframework.statemachine.rxjava3
+
+import com.safframework.statemachine.StateMachine
+import com.safframework.statemachine.context.StateContext
+import com.safframework.statemachine.interceptor.Interceptor
+import com.safframework.statemachine.model.TransitionEvent
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.statemachine.`RxJava+Extensions`
+ * @author: Tony Shen
+ * @date: 2020-02-15 17:30
+ * @version: V1.0 <描述当前版本功能>
+ */
+
+val StateMachine.stateObservable: Observable<TransitionEvent>
+    get() = Observable.create { emitter ->
+        val rxInterceptor = RxInterceptor(emitter)
+        registerInterceptor(rxInterceptor)
+        emitter.setCancellable {
+            unregisterInterceptor(rxInterceptor)
+        }
+    }
+
+val StateMachine.enterTransitionObservable: Observable<TransitionEvent.EnterTransition>
+    get() = stateObservable
+        .filter { event -> event is TransitionEvent.EnterTransition }
+        .map { event -> event as TransitionEvent.EnterTransition }
+
+val StateMachine.exitTransitionObservable: Observable<TransitionEvent.ExitTransition>
+    get() = stateObservable
+        .filter { event -> event is TransitionEvent.ExitTransition }
+        .map { event -> event as TransitionEvent.ExitTransition }
+
+private class RxInterceptor(
+    private val emitter: ObservableEmitter<TransitionEvent>
+) : Interceptor {
+
+    override fun enteringState(
+        stateMachine: StateMachine,
+        stateContext: StateContext
+    ) = emitter.onNext(TransitionEvent.EnterTransition(stateMachine, stateContext))
+
+    override fun enteredState(
+        stateMachine: StateMachine,
+        stateContext: StateContext
+    ) = emitter.onNext(TransitionEvent.ExitTransition(stateMachine, stateContext))
+}
