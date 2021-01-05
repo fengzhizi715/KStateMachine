9f4549dd507cf21a470584087af617cd1e0253e4
fengzhizi715 <fengzhizi715@126.com>
2021-01-05 21:08:57 +0800

    update build.gradle

--- core/src/main/kotlin/com/safframework/statemachine/StateMachine.kt
+++ core/src/main/kotlin/com/safframework/statemachine/StateMachine.kt
@@ -22,7 +22,9 @@ import java.util.concurrent.atomic.AtomicBoolean
  */
 class StateMachine private constructor(var name: String?=null,private val initialState: BaseState) {
 
+    @Volatile
     private lateinit var currentState: State    // 当前状态
+
     private val states = mutableListOf<State>() // 状态的列表
     private val initialized = AtomicBoolean(false)            // 是否初始化，保证状态机只初始化一次
     private var globalInterceptor: GlobalInterceptor?=null               // 全局的拦截器
