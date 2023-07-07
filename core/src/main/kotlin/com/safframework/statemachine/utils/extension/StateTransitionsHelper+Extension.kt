05373f1c2d59c61bc96774c1999c741ecd664d25
Tony Shen <fengzhizi715@126.com>
2023-07-07 11:33:57 +0800

    update

--- core/src/main/kotlin/com/safframework/statemachine/utils/extension/StateTransitionsHelper+Extension.kt
+++ core/src/main/kotlin/com/safframework/statemachine/utils/extension/StateTransitionsHelper+Extension.kt
@@ -16,13 +16,13 @@ import com.safframework.statemachine.transition.EventMatcher.Companion.isInstanc
  * @date: 2023/7/4 16:29
  * @version: V1.0 <描述当前版本功能>
  */
-fun com.safframework.statemachine.StateTransitionsHelper.requireTransition(name: String) =
+fun StateTransitionsHelper.requireTransition(name: String) =
     requireNotNull(findTransition(name)) { "Transition $name not found" }
 
 /**
  * Find transition by Event type. This might be used to start listening to transition after state machine setup.
  */
-inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHelper.findTransition(): Transition<E>? {
+inline fun <reified E : Event> StateTransitionsHelper.findTransition(): Transition<E>? {
     @Suppress("UNCHECKED_CAST")
     return transitions.find { it.eventMatcher.eventClass == E::class } as Transition<E>?
 }
@@ -30,13 +30,13 @@ inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHel
 /**
  * Require transition by Event type
  */
-inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHelper.requireTransition() =
+inline fun <reified E : Event> StateTransitionsHelper.requireTransition() =
     requireNotNull(findTransition<E>()) { "Transition for ${E::class.simpleName} not found" }
 
 /**
  * Shortcut overload for transition with an optional target state
  */
-inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHelper.transition(
+inline fun <reified E : Event> StateTransitionsHelper.transition(
     name: String? = null,
     targetState: State? = null
 ): Transition<E> = addTransition(DefaultTransition(name, isInstanceOf(), toState(), targetState))
@@ -47,7 +47,7 @@ inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHel
  *
  * This is a special kind of conditional transition but with simpler syntax and less flexibility.
  */
-inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHelper.transition(
+inline fun <reified E : Event> StateTransitionsHelper.transition(
     name: String? = null,
     block: UnitGuardedTransitionBuilder<E>.() -> Unit,
 ): Transition<E> {
@@ -66,7 +66,7 @@ inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHel
  *
  * This is a special kind of conditional transition but with simpler syntax and less flexibility.
  */
-inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHelper.transitionOn(
+inline fun <reified E : Event> StateTransitionsHelper.transitionOn(
     name: String? = null,
     block: UnitGuardedTransitionOnBuilder<E>.() -> Unit,
 ): Transition<E> {
@@ -81,7 +81,7 @@ inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHel
  * Creates conditional transition. Caller should specify lambda which calculates [TransitionDirection].
  * For example target state may be different depending on some condition.
  */
-inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHelper.transitionConditionally(
+inline fun <reified E : Event> StateTransitionsHelper.transitionConditionally(
     name: String? = null,
     block: ConditionalTransitionBuilder<E>.() -> Unit,
 ): Transition<E> {
@@ -96,7 +96,7 @@ inline fun <reified E : Event> com.safframework.statemachine.StateTransitionsHel
  * Shortcut function for type safe argument transition.
  * Data transition can not be targetless as it does not make sense.
  */
-inline fun <reified E : DataEvent<D>, D> com.safframework.statemachine.StateTransitionsHelper.dataTransition(
+inline fun <reified E : DataEvent<D>, D> StateTransitionsHelper.dataTransition(
     name: String? = null,
     targetState: DataState<D>
 ): Transition<E> {
@@ -109,7 +109,7 @@ inline fun <reified E : DataEvent<D>, D> com.safframework.statemachine.StateTran
 /**
  * Creates type safe argument transition to [DataState].
  */
-inline fun <reified E : DataEvent<D>, D> com.safframework.statemachine.StateTransitionsHelper.dataTransition(
+inline fun <reified E : DataEvent<D>, D> StateTransitionsHelper.dataTransition(
     name: String? = null,
     block: DataGuardedTransitionBuilder<E, D>.() -> Unit,
 ): Transition<E> {
@@ -129,7 +129,7 @@ inline fun <reified E : DataEvent<D>, D> com.safframework.statemachine.StateTran
 /**
  * Data transition, otherwise same as [transitionOn]
  */
-inline fun <reified E : DataEvent<D>, D> com.safframework.statemachine.StateTransitionsHelper.dataTransitionOn(
+inline fun <reified E : DataEvent<D>, D> StateTransitionsHelper.dataTransitionOn(
     name: String? = null,
     block: DataGuardedTransitionOnBuilder<E, D>.() -> Unit,
 ): Transition<E> {
