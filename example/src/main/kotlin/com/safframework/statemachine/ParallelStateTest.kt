c402bbfd263a99bb7d0d6dfd50fd5ee5dd8931dc
Tony Shen <fengzhizi715@126.com>
2023-08-16 14:47:51 +0800

    Create ParallelStateTest.kt

---
+++ example/src/main/kotlin/com/safframework/statemachine/ParallelStateTest.kt
@@ -0,0 +1,51 @@
+package com.safframework.statemachine
+
+import com.safframework.statemachine.domain.ChildMode
+import com.safframework.statemachine.domain.Event
+import com.safframework.statemachine.state.State
+import com.safframework.statemachine.statemachine.StateMachine
+import com.safframework.statemachine.statemachine.createStateMachine
+import com.safframework.statemachine.utils.extension.*
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.statemachine.ParallelStateTest
+ * @author: Tony Shen
+ * @date: 2023/8/16 14:26
+ * @version: V1.0 <描述当前版本功能>
+ */
+class ParallelTransitionEvent1: Event
+class ParallelTransitionEvent2: Event
+
+fun main() {
+
+    val sm = createStateMachine("parallel state") {
+        logger = StateMachine.Logger { println(it) }
+
+        lateinit var state1: State
+        lateinit var nested11: State
+        lateinit var nested12: State
+
+        val finalState = finalState("final")
+
+        val init = initialState("init") {
+
+            transitionOn<ParallelTransitionEvent1> {
+                targetState = { state1 }
+            }
+        }
+
+        state1 = state("Top level 1",childMode = ChildMode.PARALLEL) {
+            nested11 = state("Nested 11")
+            nested12 = state("Nested 12")
+
+            transition<ParallelTransitionEvent2> {
+                targetState = finalState
+            }
+        }
+    }
+
+    sm.sendEvent(ParallelTransitionEvent1())
+    sm.sendEvent(ParallelTransitionEvent2())
+}
